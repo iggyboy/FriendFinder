3bb6f1c6ee3d1dcbcc1561d223507d6c5e04c726
William Lukas <iggyboy456@gmail.com>
2019-10-14 11:51:25 -0500

    it wooooooorks

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,12 @@ module.exports = function (app) {
     let path = require("path");
     let friends = require("../data/friends.js");
 
+    //just returns the friends array
     app.get('/api/friends', function (req, res) {
         res.send(JSON.stringify(friends));
     });
 
+    //finds the friend with the lowest differnce in answers, and returns them to the survey html
     app.post('/api/friends', function (req, res) {
         let differences = [];
         let newDude = req.body;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,12 @@
 module.exports = function(app){
     let path = require("path");
 
+    //route for default home page
     app.get('/', function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+    //route for survey
     app.get('/survey', function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
